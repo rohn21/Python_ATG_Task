bb40cf6bd6428399fbf2d7b77bf64b820d840da4
Rohan_21 <rohn2100@gmail.com>
2024-07-10 16:14:03 +0530

    updated settings.py

--- role_based_access/usr_role/models.py
+++ role_based_access/usr_role/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    phone_number = models.CharField(max_length=20)
+    school_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    phone_number = models.CharField(max_length=20)
+    subject_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
