4d155779a1401da9a0e88f8b32f30def6e64d566
Rohan_21 <rohn2100@gmail.com>
2024-08-09 15:44:08 +0530

    add vercel app

--- role_based_access/role_based_access/settings.py
+++ role_based_access/role_based_access/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e!^&mrsq4^oy4&+5nc4@gih-eg6ter6v)=jgq_a6m+zfx+yzi6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- role_based_access/role_based_access/wsgi.py
+++ role_based_access/role_based_access/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'role_based_access.settings')
 
 application = get_wsgi_application()
+
+app = application
